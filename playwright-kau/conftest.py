1c52915b047e43ff01fc472e64c113d03962d3c4
Vishnupriya Jaganathan <126341574+VishnupriyaJaganathan@users.noreply.github.com>
2026-03-25 09:09:47 +0100

    Update conftest.py

---
+++ playwright-kau/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def base_url():
+    return "https://www.kau.se"
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "viewport": {"width": 1280, "height": 720},
+    }
